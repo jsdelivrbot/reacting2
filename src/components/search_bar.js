3bc23a8771a83e3647e211b636b6ec506c2e7269
Felipe Andrade <felipesgandrade@gmail.com>
2017-12-07 13:29:46 -0200

    initial search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+  }
+
+  render() {
+    return <input
+      value={this.state.term}
+      onChange={e => this.onTextChange(e)} />
+  }
+
+  onTextChange(event) {
+    this.setState({
+      term: event.target.value
+    })
+  }
+}
+
+export default SearchBar
