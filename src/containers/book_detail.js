3fa00d7291b95be2498f704b029d8ce3bbfa4125
Felipe Andrade <felipesgandrade@gmail.com>
2017-12-18 09:54:04 -0200

    the whole book thing

---
+++ src/containers/book_detail.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class BookDetail extends Component {
+  render() {
+    if(!this.props.activeBook)
+      return <div>Select a book to get started.</div>
+
+    return (
+      <div>{this.props.activeBook.title}</div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    activeBook: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail)
